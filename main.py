65ac3e235bf3289729be2f1b0262d677a81aeed2
VoltedSwitch <wirelessvoltswitch@gmail.com>
2024-07-02 03:26:25 +0000

    reversed the unnecessary change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def incolor(string):
     import random
-    user_input = str(input(string))
+    user_input = input(string)
     colors = ["\033[38;5;196m", "\033[38;5;208m", "\033[38;5;226m", "\033[38;5;46m", 
               "\033[38;5;21m", "\033[1;36m", "\033[1;35m"] 
     reset_color = "\033[0m"
